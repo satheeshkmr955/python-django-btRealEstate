75796e0ce731b32f5389a5fc3fdf5020f6170cb1
satheeshkumar <satheeshkmr955@gmail.com>
2020-12-07 10:38:02 +0530

    added search page and it's functionality

--- pages/views.py
+++ pages/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 
 from listings.models import Listing
 from realtors.models import Realtor
+from listings.choices import bedroom_choices, price_choices, state_choices
 
 
 def index(request):
@@ -10,7 +11,12 @@ def index(request):
     listings = Listing.objects.order_by(
         '-list_date').filter(is_published=True)[:3]
 
-    context = {"listings": listings}
+    context = {
+        "listings": listings,
+        "bedroom_choices": bedroom_choices,
+        "price_choices": price_choices,
+        "state_choices": state_choices
+    }
 
     return render(request, 'pages/index.html', context)
 
